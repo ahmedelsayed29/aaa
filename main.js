3ce808bf5f88c61ec5ce9003fa1e3063b0c3562b
Ahmed Elsayed <ahmedtahaa287@gmail.com>
2025-06-27 14:30:07 +0300

    Update main.js

--- main.js
+++ main.js
@@ -1,5 +1,12 @@
 const menuToggle = document.getElementById('menu-toggle');
 const navLinks = document.querySelector('.nav-links');
+
 menuToggle.addEventListener('click', () => {
     navLinks.classList.toggle('active');
+});
+
+navLinks.addEventListener('click', (event) => {
+    if (event.target !== navLinks) {
+        navLinks.classList.remove('active');
+    }
 });
